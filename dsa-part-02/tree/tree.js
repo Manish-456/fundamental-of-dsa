1de3e2e008842787168dbe741130608f6632ea6f
Manish <tamangmanish3456@gmail.com>
2023-10-06 21:17:00 +0545

    added overview of tree data structure

---
+++ dsa-part-02/tree/tree.js
@@ -0,0 +1,25 @@
+// ? TREE
+
+/* -> A tree is a hierarchical data structure that consists of nodes connected by edges.
+
+   -> A tree is a non-linear data structure, compared to arrays, linked lists, stacks and queues which are linear data structure
+
+   -> In linear data structures, the time required to search is proportional to the size of the data set.
+
+   -> Trees however, owing to the non-linear nature, allow quicker and easier access to the data
+
+   -> A tree will not contain any loops or cycles.
+
+*/
+ 
+// ? TREE USAGE
+/* -> File systems for directory structure
+   -> A family tree
+   -> An organization tree
+   -> DOM
+   -> Chat bots
+   -> Abstract syntax trees
+   etc.
+*/
+
+
